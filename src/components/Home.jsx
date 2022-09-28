857025191050b8f1e0d7b77d0283574e1b47b50e
Leandro Sabus <leandro.sabus@gmail.com>
2022-09-28 10:50:45 -0300

    arreglo home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 
+
 export const Home = () => {
+
     return (
         <>
 
 
             <div className='home-container animate__animated animate__fadeIn'>
 
+                <div className='img-home'>
+                    <div className='home-text'>
+                        <p>Esta página fue creada para agrupar los posibles representantes
+                            de nuestra selección Argentina de fútbol <br />
+                            en la copa del mundo de este año.
+                            Esta lista puede cambiar con el tiempo, solo es una humilde ilustración.</p>
 
+                    </div>
 
-                <div className='home-text'>
-                    <p>Esta página fue creada para agrupar los posibles representantes
-                        de nuestra selección Argentina de fútbol <br />
-                        en la copa del mundo de este año.
-                        Esta lista puede cambiar con el tiempo, solo es una humilde ilustración.</p>
                 </div>
             </div>
 
